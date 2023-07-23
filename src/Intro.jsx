02b5cf4604db296ad25fdb99cb30ca4d7ffb3ff4
Brandon <brandonkly@ucla.edu>
2023-07-22 22:47:14 -0700

    tailwind and some cleanup

---
+++ src/Intro.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+function Intro() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Sooshi</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default Intro
