2cbab1905094a5a546ffd68583c34cd119cf6916
Brandon <brandonkly@ucla.edu>
2023-07-27 22:59:41 -0700

    api gateway/lambda setup

--- server/server.js
+++ server/server.js
@@ -15,12 +15,13 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  // res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.send('Hello world!');
 });
 
 
