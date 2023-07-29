5d1f90d2c9f6f68f15827a1a277cb3570532a5cd
Brandon <brandonkly@ucla.edu>
2023-07-28 18:32:22 -0700

    user auth and .env

--- server/src/server.js
+++ server/src/server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+// Dotenv config
+const dotenv = require('dotenv');
 
 // logger middleware
 const morgan = require("morgan")
@@ -9,6 +11,17 @@ app.use(require("./routes"))
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+dotenv.config();
+dynamoose.aws.sdk.config.update({
+    "accessKeyId": process.env.AWS_ACCESS_KEY_ID,
+    "secretAccessKey": process.env.AWS_SECRET_ACCESS_KEY,
+    "region": "us-east-1",
+});
+
+// Dynamoose configuration
+const dynamoose = require("dynamoose")
+dynamoose.aws.sdk.config.update({"region": "us-east-1"})
 
 // if (process.env.NODE_ENV === 'production') {
 //   app.use(express.static(path.join(__dirname, '../client/build')));
