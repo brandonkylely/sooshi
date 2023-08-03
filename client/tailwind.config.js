af951d600d2a7692bb93c45ab6714b550d7e1e4e
Brandon <brandonkly@ucla.edu>
2023-08-02 21:50:23 -0700

    homepage hero

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -11,6 +11,10 @@ export default {
         'cour': ['Courgette', 'cursive'],
         'sig': ['Sigmar', 'cursive']
       },
+      backgroundImage: {
+        'hero-pattern': "url('./src/assets/sooshi-hero.jpg')",
+        // 'footer-texture': "url('/img/footer-texture.png')",
+      }
     },
   },
   plugins: [],
