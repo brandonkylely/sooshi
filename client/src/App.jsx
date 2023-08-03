e0027505cb587009be060724243cdde1bbf2fef3
Brandon <brandonkly@ucla.edu>
2023-08-02 21:10:38 -0700

    dependencies and login form

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,11 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 
-const client = new ApolloClient({
-  uri: '/graphql',
-  cache: new InMemoryCache(),
-});
-
 function App() {
 
   return (
-    <ApolloProvider client={client}>
       <Router>
         <Routes>
           <Route path="/" element={<HomePage/>} />
@@ -20,7 +13,6 @@ function App() {
           <Route path="/signup" element={<SignupPage />} />
         </Routes>
       </Router>
-    </ApolloProvider>
   );
 }
 
