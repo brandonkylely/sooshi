2954020674ef734ac1afd28c8f57f394a94fe875
Brandon <brandonkly@ucla.edu>
2023-08-04 16:00:29 -0700

    some form functionality

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -48,6 +48,7 @@ function Login() {
             id="exampleInputEmail2"
             aria-describedby="emailHelp"
             placeholder="Enter email"
+            autoComplete="email"
           />
           <label
             htmlFor="exampleInputEmail2"
@@ -64,6 +65,7 @@ function Login() {
             className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
             id="exampleInputPassword2"
             placeholder="Password"
+            autoComplete="current-password"
           />
           <label
             htmlFor="exampleInputPassword2"
