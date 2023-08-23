b0ba5cf399c3f978f60eec175a094586792cc2fb
Brandon <brandonkly@ucla.edu>
2023-08-22 22:59:58 -0700

    new sdk s3 client

--- server/src/controllers/Auth.controller.js
+++ server/src/controllers/Auth.controller.js
@@ -1,6 +1,6 @@
 const authService = require("../services/Auth.service");
 const Sushi = require("../models/Sushi.model");
-const AWS = require("@aws-sdk/client-s3");
+const {PutObjectCommand, S3Client} = require("@aws-sdk/client-s3");
 
 // Register New User
 exports.register = async function (req, res) {
@@ -52,38 +52,42 @@ exports.protected = async function (req, res) {
 exports.uploadImage = async function (req, res) {
   // Now creating the S3 instance which will be used in uploading photo to S3 bucket
 
-  const s3 = new AWS.S3({
+  const client = new S3Client({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: "us-west-1",
   });
 
   console.log(req.file); // check data in console that is being uploaded
 
   try {
+
     if (req.file) {
-      const params = {
+      const params = new PutObjectCommand({
         Bucket: process.env.AWS_BUCKET_NAME, // bucket name
         Key: req.file.originalname, // name of image
         Body: req.file.buffer, // body which will contain the image in buffer format
         ACL: "public-read", // defining the permissions to get public link
         ContentType: "image/jpeg", // necessary to define the image content-type to view photo in browswer with link
-      };
+      });
+
+      // uploading photo using s3 instance and saving link in database
+      const data = await client.send(params);
+
+      // if not then below code will be executed
+
+      console.log(data); // will give information about object in which photo is stored
+      // save info in database
+      const sushi = await Sushi.create({
+        userId: req.user.id,
+        title: req.body.title,
+        image: data.Location,
+      });
+
+      res.status(200).json(sushi);
+    } else {
+      return res.status(400).json({ message: "No file uploaded." });
     }
-
-    // uploading photo using s3 instance and saving link in database
-    const data = await s3.upload(params).promise();
-
-    // if not then below code will be executed
-
-    console.log(data); // will give information about object in which photo is stored
-    // save info in database
-    const sushi = await Sushi.create({
-      userId: req.user.id,
-      title: req.body.title,
-      image: data.Location,
-    });
-
-    res.status(200).json(sushi);
   } catch (err) {
     console.log(err);
     res.send(err);
