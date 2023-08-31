5b08b5ef1f7575b9ea69a010c393db9a78b9b060
Brandon <brandonkly@ucla.edu>
2023-08-30 23:08:11 -0700

    toast on invalid login

--- client/src/components/LoginForm.jsx
+++ client/src/components/LoginForm.jsx
@@ -21,6 +21,10 @@ function LoginForm() {
   };
 
   const setUser = useSetAtom(userAtom);
+  const [error, setError] = useState(false);
+  const handleRemoveError = () => {
+    setError(false);
+  };
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
@@ -30,42 +34,103 @@ function LoginForm() {
       // Store token in local storage;
       token.login(data.token);
       const user = token.decode(data.token);
-      
+
       // Update user state
       setUser(user);
+
+      setError(false);
       
-      //maybe redirect to home page?
+      // Redirect to feed page
       navigate("/feed");
     } catch (err) {
       console.log(err);
-      //failed, what do?
-      //maybe some error handling? display to user?
+      // Set Error to trigger toast
+      setError(true);
     }
   };
 
-  
   return (
     <div className="block max-w-sm rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
-      <form 
-      onSubmit={handleFormSubmit}
-      >
-          {/*username input*/}
-          <div className="relative mb-6" data-te-input-wrapper-init>
-            <input
-              type="username"
-              name="username"
-              className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-              id="exampleInput124"
-              placeholder="Username"
-              onChange={handleFormChange}
-            />
-            <label
-              htmlFor="exampleInput124"
-              className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200"
-            >
-              Username
-            </label>
+      {error && (
+        <div
+          className="pointer-events-auto mx-auto mb-4 hidden w-96 max-w-full rounded-lg bg-warning-100 bg-clip-padding text-sm text-warning-700 shadow-lg shadow-black/5 data-[te-toast-show]:block data-[te-toast-hide]:hidden"
+          id="static-example"
+          role="alert"
+          aria-live="assertive"
+          aria-atomic="true"
+          data-te-autohide="false"
+          data-te-toast-init
+          data-te-toast-show
+        >
+          <div className="flex items-center justify-between rounded-t-lg border-b-2 border-warning-200 bg-warning-100 bg-clip-padding px-4 pb-2 pt-2.5 text-warning-700">
+            <p className="flex items-center font-bold text-warning-700">
+              <svg
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="fas"
+                data-icon="exclamation-triangle"
+                className="mr-2 h-4 w-4 fill-current"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 576 512"
+              >
+                <path
+                  fill="currentColor"
+                  d="M569.517 440.013C587.975 472.007 564.806 512 527.94 512H48.054c-36.937 0-59.999-40.055-41.577-71.987L246.423 23.985c18.467-32.009 64.72-31.951 83.154 0l239.94 416.028zM288 354c-25.405 0-46 20.595-46 46s20.595 46 46 46 46-20.595 46-46-20.595-46-46-46zm-43.673-165.346l7.418 136c.347 6.364 5.609 11.346 11.982 11.346h48.546c6.373 0 11.635-4.982 11.982-11.346l7.418-136c.375-6.874-5.098-12.654-11.982-12.654h-63.383c-6.884 0-12.356 5.78-11.981 12.654z"
+                ></path>
+              </svg>
+              Login failed
+            </p>
+            <div className="flex items-center">
+              <button
+                type="button"
+                className="ml-2 box-content rounded-none border-none opacity-80 hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
+                data-te-toast-dismiss
+                aria-label="Close"
+                onClick={handleRemoveError}
+              >
+                <span className="w-[1em] focus:opacity-100 disabled:pointer-events-none disabled:select-none disabled:opacity-25 [&.disabled]:pointer-events-none [&.disabled]:select-none [&.disabled]:opacity-25">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="h-6 w-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </span>
+              </button>
+            </div>
           </div>
+          <div className="break-words rounded-b-lg bg-warning-100 px-4 py-4 text-warning-700">
+            Please ensure that your username and password are correct.
+          </div>
+        </div>
+      )}
+      <form onSubmit={handleFormSubmit}>
+        {/*username input*/}
+        <div className="relative mb-6" data-te-input-wrapper-init>
+          <input
+            type="username"
+            name="username"
+            className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+            id="exampleInput124"
+            placeholder="Username"
+            onChange={handleFormChange}
+          />
+          <label
+            htmlFor="exampleInput124"
+            className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200"
+          >
+            Username
+          </label>
+        </div>
 
         {/*Password input*/}
         <div className="relative mb-6" data-te-input-wrapper-init>
