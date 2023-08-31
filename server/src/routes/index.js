5b2fc64f27c131421344e8b8e0327b0b1810b3ac
Brandon <brandonkly@ucla.edu>
2023-08-30 23:28:53 -0700

    last update log

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const authRoutes = require('./Auth.routes')
 
 router.get('/', (req, res) => {
-  res.send('Hello world!')
+  res.send('Last Updated: 8/30/2023')
 })
 
 // Import Auth routes
