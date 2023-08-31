7b3500ca843771460b8c86fdb9ffe06f1cc3fd5c
Brandon <brandonkly@ucla.edu>
2023-08-30 18:08:24 -0700

    pagination styling

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -1,10 +1,10 @@
 function Pagination() {
   return (
     <nav aria-label="Page navigation example">
-    <ul className="list-style-none flex">
+    <ul className="list-style-none flex justify-center my-3">
       <li>
         <a
-          className="relative block rounded bg-transparent px-3 py-1.5 text-sm text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
+          className="relative block rounded bg-transparent px-3 py-1.5 text-md text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
           href="#"
         >
           Previous
@@ -12,7 +12,7 @@ function Pagination() {
       </li>
       <li>
         <a
-          className="relative block rounded bg-transparent px-3 py-1.5 text-sm text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
+          className="relative block rounded bg-transparent px-3 py-1.5 text-md text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
           href="#"
         >
           1
@@ -20,7 +20,7 @@ function Pagination() {
       </li>
       <li aria-current="page">
         <a
-          className="relative block rounded bg-transparent px-3 py-1.5 text-sm text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
+          className="relative block rounded bg-transparent px-3 py-1.5 text-md text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
           href="#"
         >
           2
@@ -28,7 +28,7 @@ function Pagination() {
       </li>
       <li>
         <a
-          className="relative block rounded bg-transparent px-3 py-1.5 text-sm text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
+          className="relative block rounded bg-transparent px-3 py-1.5 text-md text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
           href="#"
         >
           3
@@ -36,7 +36,7 @@ function Pagination() {
       </li>
       <li>
         <a
-          className="relative block rounded bg-transparent px-3 py-1.5 text-sm text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
+          className="relative block rounded bg-transparent px-3 py-1.5 text-md text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
           href="#"
         >
           Next
