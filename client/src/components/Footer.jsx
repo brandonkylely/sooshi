c6a5a33cefd614d5e2d7cd5228b06e360ef02691
Brandon <brandonkly@ucla.edu>
2023-09-01 23:22:35 -0700

    footer info

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
       <footer className="bg-neutral-100 text-center text-neutral-600 dark:bg-neutral-600 dark:text-neutral-200 lg:text-left">
         <div className="flex items-center justify-center border-b-2 border-neutral-200 p-6 dark:border-neutral-500 lg:justify-between">
           <div className="mr-12 hidden lg:block">
-            <span>Get connected with us on social networks:</span>
+            <span>Check out my social networks!</span>
           </div>
           {/* Social network icons container */}
           <div className="flex justify-center">
@@ -105,48 +105,46 @@ function Footer() {
                 >
                   <path d="M12.378 1.602a.75.75 0 00-.756 0L3 6.632l9 5.25 9-5.25-8.622-5.03zM21.75 7.93l-9 5.25v9l8.628-5.032a.75.75 0 00.372-.648V7.93zM11.25 22.18v-9l-9-5.25v8.57a.75.75 0 00.372.648l8.628 5.033z" />
                 </svg>
-                Tailwind ELEMENTS
+                Sooshi
               </h6>
               <p>
-                Here you can use rows and columns to organize your footer
-                content. Lorem ipsum dolor sit amet, consectetur adipisicing
-                elit.
+                Hey! This application is still in active development. If you&apos;d like to follow along, please check out my repository for updates. Thanks for visiting!
               </p>
             </div>
             {/* Products section */}
             <div className="">
               <h6 className="mb-4 flex justify-center font-semibold uppercase md:justify-start">
-                Products
+                Developer Links
               </h6>
               <p className="mb-4">
                 <a href="#!" className="text-neutral-600 dark:text-neutral-200">
-                  Angular
+                  Sooshi Repository
                 </a>
               </p>
               <p className="mb-4">
                 <a href="#!" className="text-neutral-600 dark:text-neutral-200">
-                  React
+                  Sooshi Kanban Board
                 </a>
               </p>
               <p className="mb-4">
                 <a href="#!" className="text-neutral-600 dark:text-neutral-200">
-                  Vue
+                  Developer Portfolio
                 </a>
               </p>
-              <p>
+              {/* <p>
                 <a href="#!" className="text-neutral-600 dark:text-neutral-200">
-                  Laravel
+                  Lorem
                 </a>
-              </p>
+              </p> */}
             </div>
             {/* Useful links section */}
             <div className="">
               <h6 className="mb-4 flex justify-center font-semibold uppercase md:justify-start">
-                Useful links
+                Useful Links (Coming Soon)
               </h6>
               <p className="mb-4">
                 <a href="#!" className="text-neutral-600 dark:text-neutral-200">
-                  Pricing
+                  Change Username/Password
                 </a>
               </p>
               <p className="mb-4">
@@ -156,14 +154,14 @@ function Footer() {
               </p>
               <p className="mb-4">
                 <a href="#!" className="text-neutral-600 dark:text-neutral-200">
-                  Orders
-                </a>
-              </p>
-              <p>
-                <a href="#!" className="text-neutral-600 dark:text-neutral-200">
                   Help
                 </a>
               </p>
+              {/* <p>
+                <a href="#!" className="text-neutral-600 dark:text-neutral-200">
+                  Help
+                </a>
+              </p> */}
             </div>
             {/* Contact section */}
             <div>
@@ -192,7 +190,7 @@ function Footer() {
                   <path d="M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z" />
                   <path d="M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z" />
                 </svg>
-                info@example.com
+                brandonkly@ucla.edu
               </p>
               <p className="mb-4 flex items-center justify-center md:justify-start">
                 <svg
@@ -209,21 +207,6 @@ function Footer() {
                 </svg>
                 + 01 234 567 88
               </p>
-              <p className="flex items-center justify-center md:justify-start">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  className="mr-3 h-5 w-5"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M7.875 1.5C6.839 1.5 6 2.34 6 3.375v2.99c-.426.053-.851.11-1.274.174-1.454.218-2.476 1.483-2.476 2.917v6.294a3 3 0 003 3h.27l-.155 1.705A1.875 1.875 0 007.232 22.5h9.536a1.875 1.875 0 001.867-2.045l-.155-1.705h.27a3 3 0 003-3V9.456c0-1.434-1.022-2.7-2.476-2.917A48.716 48.716 0 0018 6.366V3.375c0-1.036-.84-1.875-1.875-1.875h-8.25zM16.5 6.205v-2.83A.375.375 0 0016.125 3h-8.25a.375.375 0 00-.375.375v2.83a49.353 49.353 0 019 0zm-.217 8.265c.178.018.317.16.333.337l.526 5.784a.375.375 0 01-.374.409H7.232a.375.375 0 01-.374-.409l.526-5.784a.373.373 0 01.333-.337 41.741 41.741 0 018.566 0zm.967-3.97a.75.75 0 01.75-.75h.008a.75.75 0 01.75.75v.008a.75.75 0 01-.75.75H18a.75.75 0 01-.75-.75V10.5zM15 9.75a.75.75 0 00-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 00.75-.75V10.5a.75.75 0 00-.75-.75H15z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-                + 01 234 567 89
-              </p>
             </div>
           </div>
         </div>
@@ -235,7 +218,7 @@ function Footer() {
             className="font-semibold text-neutral-600 dark:text-neutral-400"
             href="https://tailwind-elements.com/"
           >
-            Tailwind Elements
+            made with ❤️ by Brandon Ly
           </a>
         </div>
       </footer>
