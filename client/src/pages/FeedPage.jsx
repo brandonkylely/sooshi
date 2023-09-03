f5b6899e4f4dae9ec89752997240a0ae35938407
Brandon <brandonkly@ucla.edu>
2023-09-02 20:09:36 -0700

    added loading error toast

--- client/src/pages/FeedPage.jsx
+++ client/src/pages/FeedPage.jsx
@@ -41,7 +41,6 @@ function FeedPage() {
       setSushiData(data);
       console.log("SUSHI DATA", sushiData[0]);
 
-      setError(false);
     } catch (err) {
       console.log(err);
       // Set Error to trigger toast
@@ -66,6 +65,11 @@ function FeedPage() {
   const handleNavigate = () => {
     navigate("/post");
   };
+
+  const handleRemoveError = () => {
+    setError(false);
+  };
+
   return (
     <div>
       <Navbar />
@@ -83,6 +87,68 @@ function FeedPage() {
           Post
         </button>
       </div>
+      {error && (
+        <div
+          className="pointer-events-auto mx-auto mb-4 hidden w-96 max-w-full rounded-lg bg-warning-100 bg-clip-padding text-sm text-warning-700 shadow-lg shadow-black/5 data-[te-toast-show]:block data-[te-toast-hide]:hidden"
+          id="static-example"
+          role="alert"
+          aria-live="assertive"
+          aria-atomic="true"
+          data-te-autohide="false"
+          data-te-toast-init
+          data-te-toast-show
+        >
+          <div className="flex items-center justify-between rounded-t-lg border-b-2 border-warning-200 bg-warning-100 bg-clip-padding px-4 pb-2 pt-2.5 text-warning-700">
+            <p className="flex items-center font-bold text-warning-700">
+              <svg
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="fas"
+                data-icon="exclamation-triangle"
+                className="mr-2 h-4 w-4 fill-current"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 576 512"
+              >
+                <path
+                  fill="currentColor"
+                  d="M569.517 440.013C587.975 472.007 564.806 512 527.94 512H48.054c-36.937 0-59.999-40.055-41.577-71.987L246.423 23.985c18.467-32.009 64.72-31.951 83.154 0l239.94 416.028zM288 354c-25.405 0-46 20.595-46 46s20.595 46 46 46 46-20.595 46-46-20.595-46-46-46zm-43.673-165.346l7.418 136c.347 6.364 5.609 11.346 11.982 11.346h48.546c6.373 0 11.635-4.982 11.982-11.346l7.418-136c.375-6.874-5.098-12.654-11.982-12.654h-63.383c-6.884 0-12.356 5.78-11.981 12.654z"
+                ></path>
+              </svg>
+              Oops!
+            </p>
+            <div className="flex items-center">
+              <button
+                type="button"
+                className="ml-2 box-content rounded-none border-none opacity-80 hover:no-underline hover:opacity-75 focus:opacity-100 focus:shadow-none focus:outline-none"
+                data-te-toast-dismiss
+                aria-label="Close"
+                onClick={handleRemoveError}
+              >
+                <span className="w-[1em] focus:opacity-100 disabled:pointer-events-none disabled:select-none disabled:opacity-25 [&.disabled]:pointer-events-none [&.disabled]:select-none [&.disabled]:opacity-25">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="h-6 w-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                </span>
+              </button>
+            </div>
+          </div>
+          <div className="break-words rounded-b-lg bg-warning-100 px-4 py-4 text-warning-700">
+            An error occured while loading the feed.
+          </div>
+        </div>
+      )}
       <div className="grid-cols-1 sm:grid md:grid-cols-2 ">
         {sushiData.map((sushi, index) => (
           <SushiCard  key={index} title={sushi.title} image={sushi.signedURL} />
