d028d5aed4c08cffef2494e8f3e4f022036d2308
Brandon <brandonkly@ucla.edu>
2023-09-02 22:35:19 -0700

    forward pagination working on insomnia

--- server/src/controllers/Auth.controller.js
+++ server/src/controllers/Auth.controller.js
@@ -1,8 +1,13 @@
 const authService = require("../services/Auth.service");
 const Sushi = require("../models/Sushi.model");
 const User = require("../models/User.model");
-const {PutObjectCommand, GetObjectCommand, S3Client} = require("@aws-sdk/client-s3");
+const {
+  PutObjectCommand,
+  GetObjectCommand,
+  S3Client,
+} = require("@aws-sdk/client-s3");
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+const { parse } = require("uuid");
 
 // Register New User
 exports.register = async function (req, res) {
@@ -61,20 +66,23 @@ exports.upload = async function (req, res) {
   });
 
   try {
-    const userData = await User.scan({username: {eq: req.body.decodedUID}}).exec();
+    const userData = await User.scan({
+      username: { eq: req.body.decodedUID },
+    }).exec();
 
     if (req.file) {
       // Create unique keys for each photo
-      const username = req.body.decodedUID ? req.body.decodedUID : req.body.userId
-      const datetime = Date.now().toString() 
+      const username = req.body.decodedUID
+        ? req.body.decodedUID
+        : req.body.userId;
+      const datetime = Date.now().toString();
       const key = `${username}_${datetime}_${req.file.originalname}`;
-
 
       const params = new PutObjectCommand({
         Bucket: process.env.AWS_BUCKET_NAME, // bucket name
         Key: key, // name of image
         Body: req.file.buffer, // body which will contain the image in buffer format
-        ACL: 'bucket-owner-full-control',
+        ACL: "bucket-owner-full-control",
         ContentType: "image/jpeg", // necessary to define the image content-type to view photo in browswer with link
       });
 
@@ -103,51 +111,56 @@ exports.upload = async function (req, res) {
   }
 };
 
-
-
 /**
  * get Sushi Feed
  * TODO: Add pagination
  * TODO: Check for sorting, might need to use .query() instead of .scan()
-*/
-exports.getSushiFeed = async function (req, res) { 
+ */
+exports.getSushiFeed = async function (req, res) {
   try {
-    // Find the latest 4 sushi posts
-    // .startAt(res.lastKey) - for pagination. lastKey is an object response from previous query
-    const sushiData = await Sushi.scan().limit(4).exec();
-    res.json(sushiData);
-  }
-  catch (err) {
+    // Pagination
+    const { lastKeyData } = req.query;
+    const lastKey = { id: lastKeyData }
+    const limit = 4;
+
+    if (lastKey){
+      const sushiData = await Sushi.scan().limit(limit).startAt(lastKey).exec();
+      return res.json(sushiData);
+    } else {
+      const sushiData = await Sushi.scan().limit(limit).exec();
+      return res.json(sushiData);
+    }
+    
+  } catch (err) {
     console.error(err);
     res.send(err);
   }
-}
+};
 
 /**
  * get Single Sushi
- * 
+ *
  */
-exports.getSushiURL = async function (req, res) { 
-    // TODO: Add support for other file types
-    const { fileName } = req.query;
-    const params = new GetObjectCommand({
-        Bucket: process.env.AWS_BUCKET_NAME,
-        Key: fileName,
-    });
+exports.getSushiURL = async function (req, res) {
+  // TODO: Add support for other file types
+  const { fileName } = req.query;
+  const params = new GetObjectCommand({
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: fileName,
+  });
 
+  const client = new S3Client({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: "us-west-1",
+  });
 
-    const client = new S3Client({
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-      region: "us-west-1",
-    });
-
-    try {
-        const signedUrl = await getSignedUrl(client, params, { expiresIn: 120 });
-        // console.log(signedUrl);
-        res.json(signedUrl)
-    } catch (err) {
-        console.error(err);
-        res.send(err);
-    }
-}
+  try {
+    const signedUrl = await getSignedUrl(client, params, { expiresIn: 120 });
+    // console.log(signedUrl);
+    res.json(signedUrl);
+  } catch (err) {
+    console.error(err);
+    res.send(err);
+  }
+};
