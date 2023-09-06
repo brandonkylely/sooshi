b9daa75cb464a2d7edbcace81c1900d940b6ca2c
Brandon <brandonkly@ucla.edu>
2023-09-05 23:23:17 -0700

    redeploy log

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const authRoutes = require('./Auth.routes')
 
 router.get('/', (req, res) => {
-  res.send('Last Updated: 8/30/2023')
+  res.send('Last Updated: 9/5/2023')
 })
 
 // Import Auth routes
