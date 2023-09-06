55bfaf9beab0724574f512e9ecd7979b1810274b
Brandon <brandonkly@ucla.edu>
2023-09-05 23:22:31 -0700

    previous button no longer renders on page 1

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -12,31 +12,31 @@ function Pagination() {
 
   return (
     <nav aria-label="Page navigation example">
-    <ul className="list-style-none flex justify-center my-3">
-      <li>
-        <button
-          className="relative block rounded bg-transparent px-3 py-1.5 text-md text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
-        >
-          Previous
-        </button>
-      </li>
-      <li>
-        <button
-          className="relative block rounded bg-transparent px-3 py-1.5 text-md text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
-        >
-          {pagination && pagination.pageNumber}
-        </button>
-      </li>
-      <li>
-        <button
-          className="relative block rounded bg-transparent px-3 py-1.5 text-md text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
-          onClick={handleNextPage}
-        >
-          Next
-        </button>
-      </li>
-    </ul>
-  </nav>
+      <ul className="list-style-none flex justify-center my-3">
+        <li>
+          {pagination.pageNumber === 1 ? (
+            <div></div>
+          ) : (
+            <button className="relative block rounded bg-transparent px-3 py-1.5 text-md text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white">
+              Previous
+            </button>
+          )}
+        </li>
+        <li>
+          <button className="relative block rounded bg-transparent px-3 py-1.5 text-md text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white">
+            {pagination && pagination.pageNumber}
+          </button>
+        </li>
+        <li>
+          <button
+            className="relative block rounded bg-transparent px-3 py-1.5 text-md text-neutral-600 transition-all duration-300 hover:bg-neutral-100 dark:text-white dark:hover:bg-neutral-700 dark:hover:text-white"
+            onClick={handleNextPage}
+          >
+            Next
+          </button>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
