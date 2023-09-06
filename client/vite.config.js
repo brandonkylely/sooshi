058635849f806d12d83922d0f620eea3120742d3
Brandon <brandonkly@ucla.edu>
2023-09-05 23:56:43 -0700

    different proxy solution for deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,29 +2,39 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+const defaultConfig = {
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: import.meta.env.NODE_ENV === 'production'
-          ? "https://f997a554a1.execute-api.us-west-1.amazonaws.com/latest" // Deployed API
-          : "http://localhost:3001", // Local API
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-        configure: (proxy) => {
-          proxy.on('error', (err) => {
-            console.log('proxy error', err);
-          });
-          proxy.on('proxyReq', (req) => {
-            console.log('Sending Request to the Target:', req.method, req.url);
-          });
-          proxy.on('proxyRes', (proxyRes, req) => {
-            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
-          });
-        },
-      },
-    },
-  },
+}
+
+export default defineConfig(({ command, mode }) => {
+  if (command === 'serve') {
+    const isDev = mode === 'development'
+
+    return {
+      ...defaultConfig,
+      server: {
+        proxy: {
+          '/v1': {
+            target: isDev ? 'https://127.0.0.1:8080' : 'https://f997a554a1.execute-api.us-west-1.amazonaws.com/latest',
+            changeOrigin: isDev,
+            secure: !isDev,
+            ws: true,
+            configure: (proxy) => {
+              proxy.on('error', (err) => {
+                console.log('proxy error', err);
+              });
+              proxy.on('proxyReq', (req) => {
+                console.log('Sending Request to the Target:', req.method, req.url);
+              });
+              proxy.on('proxyRes', (proxyRes, req) => {
+                console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+              });
+            },
+          }
+        }
+      }
+    }
+  } else {
+    return defaultConfig
+  }
 })
