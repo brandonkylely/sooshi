6ed19f1f6799f29f639b9d129b6e4299925a6fd0
Brandon <brandonkly@ucla.edu>
2023-09-07 19:03:21 -0700

    vercel deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,41 +1,81 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 const defaultConfig = {
   // base: '/sooshi/',
   plugins: [react()],
-}
+};
 
 export default defineConfig(({ command, mode }) => {
-  if (command === 'serve') {
-    const isDev = mode === 'development'
+  if (command === "serve") {
+    const isDev = mode === "development";
 
     return {
       ...defaultConfig,
       server: {
         proxy: {
-          '/api': {
-            target: isDev ? 'https://localhost:3001' : 'https://f997a554a1.execute-api.us-west-1.amazonaws.com/latest',
+          "/api": {
+            target: "http://localhost:3001/",
             changeOrigin: isDev,
             secure: !isDev,
             ws: true,
             configure: (proxy) => {
-              proxy.on('error', (err) => {
-                console.log('proxy error', err);
+              proxy.on("error", (err) => {
+                console.log("proxy error", err);
               });
-              proxy.on('proxyReq', (req) => {
-                console.log('Sending Request to the Target:', req.method, req.url);
+              proxy.on("proxyReq", (req) => {
+                console.log(
+                  "Sending Request to the Target:",
+                  req.method,
+                  req.url
+                );
               });
-              proxy.on('proxyRes', (proxyRes, req) => {
-                console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+              proxy.on("proxyRes", (proxyRes, req) => {
+                console.log(
+                  "Received Response from the Target:",
+                  proxyRes.statusCode,
+                  req.url
+                );
               });
             },
-          }
-        }
-      }
-    }
+          },
+        },
+      },
+    };
   } else {
-    return defaultConfig
+    return {
+      ...defaultConfig,
+      server: {
+        proxy: {
+          "/api": {
+            target:
+              "https://f997a554a1.execute-api.us-west-1.amazonaws.com/latest",
+            changeOrigin: isDev,
+            secure: !isDev,
+            ws: true,
+            configure: (proxy) => {
+              proxy.on("error", (err) => {
+                console.log("proxy error", err);
+              });
+              proxy.on("proxyReq", (req) => {
+                console.log(
+                  "Sending Request to the Target:",
+                  req.method,
+                  req.url
+                );
+              });
+              proxy.on("proxyRes", (proxyRes, req) => {
+                console.log(
+                  "Received Response from the Target:",
+                  proxyRes.statusCode,
+                  req.url
+                );
+              });
+            },
+          },
+        },
+      },
+    };
   }
-})
+});
