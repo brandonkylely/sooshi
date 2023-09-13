0199f7cedfcf8f7348c42c2e11898f5e5ff22377
Brandon <brandonkly@ucla.edu>
2023-09-12 23:56:30 -0700

    header method

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
   if (allowedOrigins.includes(origin)) {
        res.setHeader('Access-Control-Allow-Origin', origin);
   }
-  res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.append('Access-Control-Allow-Methods', 'OPTIONS, GET,PUT,POST,DELETE');
   res.append('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
