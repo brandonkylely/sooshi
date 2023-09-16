19b1d0c7d266b67bb08439035af2e5a485be16aa
Brandon <brandonkly@ucla.edu>
2023-09-15 17:02:18 -0700

    minor comments for dev api switch

--- client/src/state/index.js
+++ client/src/state/index.js
@@ -9,6 +9,7 @@ export const paginationAtom = atom({ pageNumber: 1, lastKeyData: null });
 export const feedLoadingAtom = atom(false);
 
 // Development only
-// Check if we're in dev mode, used to render button to enable localhost API
+// Check if we're in dev mode, used to render Navbar button for dev API switch
 export const devAtom = atom(import.meta.env.DEV);
+// true = use Local API, false = use AWS API
 export const devAPIAtom = atom(false);
