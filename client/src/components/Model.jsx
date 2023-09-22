2e12dd2b1a99df17a0fc2bf0ef0d6992bc8947db
Brandon <brandonkly@ucla.edu>
2023-09-21 23:03:43 -0700

    model click animation

--- client/src/components/Model.jsx
+++ client/src/components/Model.jsx
@@ -1,9 +1,14 @@
-import { Canvas } from "@react-three/fiber";
+import { useState } from "react";
+import { Canvas, act } from "@react-three/fiber";
 import { useGLTF, ContactShadows, Float } from "@react-three/drei";
+import { useSpring, animated } from '@react-spring/three'
 
 function Model(props) {
   const { scene } = useGLTF("sushi.gltf");
-  return <primitive object={scene} {...props} />;
+  const [active, setActive] = useState(false);
+  const {rotation} = useSpring({rotation: active ? [0, Math.PI * 2, 0] : [0, 0, 0]})
+
+  return <animated.primitive rotation={rotation} onClick={() => {setActive(!active);}} object={scene} {...props} />;
 }
 
 function Scene() {
@@ -14,13 +19,13 @@ function Scene() {
           fov: 45,
           near: 0.1,
           far: 1000,
-          position: [0, 0.5, 1],
+          position: [0, 0.2, 1],
         }}
       >
         <ambientLight intensity={0.4} />
         <pointLight position={[0.1, 0.4, 0.1]} intensity={0.5} />
         <Float
-          position={[0.4, 0.1, 0]}
+          position={[0.4, 0, 0]}
           rotationIntensity={1.8}
           floatingRange={[-0.05, 0.05]}
           speed={2}
