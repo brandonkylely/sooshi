00b76a447feb791142c399f59a43e4ea46874729
Brandon <brandonkly@ucla.edu>
2023-09-21 23:13:11 -0700

    final scene edits for now, eslint disable for r3f

--- client/src/components/Model.jsx
+++ client/src/components/Model.jsx
@@ -1,5 +1,6 @@
+/* eslint-disable */
 import { useState } from "react";
-import { Canvas, act } from "@react-three/fiber";
+import { Canvas } from "@react-three/fiber";
 import { useGLTF, ContactShadows, Float } from "@react-three/drei";
 import { useSpring, animated } from '@react-spring/three'
 
@@ -8,7 +9,7 @@ function Model(props) {
   const [active, setActive] = useState(false);
   const {rotation} = useSpring({rotation: active ? [0, Math.PI * 2, 0] : [0, 0, 0]})
 
-  return <animated.primitive rotation={rotation} onClick={() => {setActive(!active);}} object={scene} {...props} />;
+  return <animated.primitive scale={1.5} rotation={rotation} onClick={() => {setActive(!active);}} object={scene} {...props} />;
 }
 
 function Scene() {
@@ -19,20 +20,21 @@ function Scene() {
           fov: 45,
           near: 0.1,
           far: 1000,
-          position: [0, 0.2, 1],
+          position: [0, 0.4, 1],
         }}
       >
         <ambientLight intensity={0.4} />
-        <pointLight position={[0.1, 0.4, 0.1]} intensity={0.5} />
+        <pointLight position={[0.35, 0.4, 0.1]} intensity={0.5} />
         <Float
           position={[0.4, 0, 0]}
+          rotation={[0, Math.PI / 2, 0]}
           rotationIntensity={1.8}
           floatingRange={[-0.05, 0.05]}
           speed={2}
         >
           <Model />
         </Float>
-        <ContactShadows position-y={-1.5} opacity={0.6} scale={5} blur={2.4} />
+        <ContactShadows position-y={-0.1} opacity={0.7} scale={1.2} blur={1.8} />
       </Canvas>
     </div>
   );
